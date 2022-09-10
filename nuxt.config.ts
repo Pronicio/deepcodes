32f6ea15214cb03b38f441b9ae59484199be1e17
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-09-10 19:33:55 +0200

    🐛 Fix config x2.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
     router: {
         base: '/deepcodes/'
     },
